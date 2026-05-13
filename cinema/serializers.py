1e8fd28de1f922eec9ef2f5682faf7b2788f6770
matheuslacerda-dev <matheuslacerda01@proton.me>
2026-05-13 17:30:43 -0300

    add id fields in serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,7 +6,7 @@ from cinema.models import Actor, CinemaHall, Genre, Movie, MovieSession
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
-        fields = ["name", "rows", "seats_in_row", "capacity"]
+        fields = ["id", "name", "rows", "seats_in_row", "capacity"]
 
 
 class ActorSerializer(serializers.ModelSerializer):
@@ -24,13 +24,11 @@ class GenreSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ["title", "description", "duration", "genres", "actors"]
+        fields = ["id", "title", "description", "duration", "genres", "actors"]
 
 
 class MovieListSerializer(MovieSerializer):
-    genres = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field="name"
-    )
+    genres = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
     actors = serializers.SlugRelatedField(
         many=True, read_only=True, slug_field="full_name"
     )
@@ -44,14 +42,12 @@ class MovieRetrieveSerializer(MovieSerializer):
 class MovieSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieSession
-        fields = ["show_time", "movie", "cinema_hall"]
+        fields = ["id", "show_time", "movie", "cinema_hall"]
 
 
 class MovieSessionListSerializer(MovieSessionSerializer):
     movie_title = serializers.CharField(source="movie.title", read_only=True)
-    cinema_hall_name = serializers.CharField(
-        source="cinema_hall.name", read_only=True
-    )
+    cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
     cinema_hall_capacity = serializers.IntegerField(
         source="cinema_hall.capacity", read_only=True
     )
